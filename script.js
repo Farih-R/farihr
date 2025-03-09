3ab6883cab12c07a247fd968d96888947c9d18c5
farih-r <faryh453@gmail.com>
2025-03-09 14:42:33 +0700

    Update halaman portofolio

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-document.getElementById("greetButton").addEventListener("click", function() {
-=======
-<<<<<<< HEAD
-document.getElementById("greetButton").addEventListener("click", function() {
-=======
-document.getElementById("greetButton").addEventListener("click", function() {
->>>>>>> 55bc202 (first commit)
->>>>>>> e757205 (Update halaman portofolio)
-    alert("Terima kasih telah mengunjungi website saya!");});
+
